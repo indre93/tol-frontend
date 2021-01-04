15f691b5a66c6e0e61f9f6fdcd2018278fcc9534
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-04 17:00:38 -0600

    import discussionsReducer func and update createStore argument w it

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
+import discussionReducer from './reducers/discussionReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(discussionReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
