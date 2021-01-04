9d0ab843c88d89882f907dee60a77a26ad5cb28b
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-04 01:44:38 -0600

    install packages and import them to index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App" >
+        App
       </div>
     );
   }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
