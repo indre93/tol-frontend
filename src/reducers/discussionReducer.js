7502a9322b068253d3e265e2ceddeb628b3acc9c
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-04 21:54:09 -0600

    add case for ADD_DISCUSSION so it returns the updated array of discussions including the new discussion created

--- src/reducers/discussionReducer.js
+++ src/reducers/discussionReducer.js
@@ -3,6 +3,11 @@ const discussionReducer = (state = { discussions: [] }, action) => {
   switch (action.type) {
     case 'FETCH_DISCUSSIONS':
       return { discussions: action.payload };
+    case 'ADD_DISCUSSION':
+      return {
+        ...state,
+        discussions: [...state.discussions, action.payload]
+      };
     default:
       return state;
   }
