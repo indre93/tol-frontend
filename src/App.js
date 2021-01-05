7f9cf5e46515625efe528c8d632c9abe780526a9
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-04 19:32:21 -0600

    import DiscussionsContainer and call component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
+import DiscussionsContainer from './containers/DiscussionsContainer';
 
 class App extends Component {
-
   render() {
     return (
       <div className="App" >
-        App
+        <DiscussionsContainer />
       </div>
     );
   }
-
 }
 
 export default App;
