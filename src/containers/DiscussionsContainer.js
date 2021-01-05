29e089cbeca90b1131a771cb7b3be0835dba45a1
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-04 20:48:34 -0600

    rename fetchDiscussions.js file to discussions.js since it will hold multiple different actions

--- src/containers/DiscussionsContainer.js
+++ src/containers/DiscussionsContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchDiscussions } from '../actions/fetchDiscussions';
+import { fetchDiscussions } from '../actions/discussions';
 import DiscussionInput from '../components/DiscussionInput';
 import Discussions from '../components/Discussions';
 
