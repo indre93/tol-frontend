97dee710b69b4cea77b371bad889133d344fcabb
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-05 16:24:07 -0600

    add ADD_COMMENT case to handle dispatched data from addComment action

--- src/reducers/discussionReducer.js
+++ src/reducers/discussionReducer.js
@@ -1,16 +1,29 @@
-
 const discussionReducer = (state = { discussions: [] }, action) => {
+
   switch (action.type) {
     case 'FETCH_DISCUSSIONS':
       return { discussions: action.payload };
+
     case 'ADD_DISCUSSION':
       return {
         ...state,
         discussions: [...state.discussions, action.payload]
       };
+
+    case 'ADD_COMMENT':
+      const discussions = state.discussions.map(discussion => {
+        if (discussion.id === action.payload.id) {
+          return action.payload;
+        } else {
+          return discussion;
+        }
+      });
+      return { ...state, discussions: discussions };
+
     default:
       return state;
   }
+
 };
 
 export default discussionReducer;
