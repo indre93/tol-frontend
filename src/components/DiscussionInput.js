dd5f853403e148cbc48f4ca544c27ae412e72ba9
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-04 19:26:21 -0600

    add discussionsInput component to handle form rendering to add discussion

---
+++ src/components/DiscussionInput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class DiscussionInput extends Component {
+  render() {
+    return (
+      <div>
+        DiscussionInput
+      </div>
+    );
+  }
+}
+
+
+export default DiscussionInput;
