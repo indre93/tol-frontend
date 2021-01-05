b135f5aae5a43c82e9d58d190fae6ed3a2c47de0
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-04 19:24:21 -0600

    fetch discussions data from server and dispatch it to be handled in reducer

--- src/actions/fetchDiscussions.js
+++ src/actions/fetchDiscussions.js
@@ -1,3 +1,9 @@
-export function fetchDiscussions() {
-
-}
+export const fetchDiscussions = () => {
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/discussions')
+      .then(resp => resp.json())
+      .then(discussions => {
+        dispatch({ type: 'FETCH_DISCUSSIONS', payload: discussions });
+      });
+  };
+};
