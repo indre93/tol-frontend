3181a99f5fecf91d7a726afdcc1deafcf0d99aaa
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-04 21:50:39 -0600

    add addDiscussion action func to handle the creation of a new discussion

--- src/actions/discussions.js
+++ src/actions/discussions.js
@@ -8,3 +8,19 @@ export const fetchDiscussions = () => {
   };
 };
 
+export const addDiscussion = (data) => {
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/discussions', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(resp => resp.json())
+      .then(discussion => {
+        dispatch({ type: 'ADD_DISCUSSION', payload: discussion });
+      });
+  };
+};
