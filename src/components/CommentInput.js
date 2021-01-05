6c15ed937c83ec2ade1d15d2dbd69b68d99418fb
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-05 16:25:26 -0600

    add form for new comments and import connect and addComment

--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addComment } from '../actions/comments';
 
 class CommentInput extends Component {
 
   render() {
     return (
       <div>
-        CommentInput
+        <h3>Add Comment</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>Username: </label>
+          <input
+            type='text'
+            name='username'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <label>Comment: </label>
+          <input
+            type='text'
+            name='content'
+            value={this.state.content}
+            onChange={this.handleChange}
+          />
+          <input type='submit' />
+        </form>
       </div>
     );
   }
 }
 
-export default CommentInput;
+export default connect(null, { addComment })(CommentInput);
