4c09e4c903b1986db44dc96e6ea23e24b5e32b03
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-05 11:06:36 -0600

    add CommentInput components and import it to CommentsContainer

--- src/containers/CommentsContainer.js
+++ src/containers/CommentsContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import CommentInput from '../components/CommentInput';
 
 class CommentsContainer extends Component {
 
   render() {
     return (
       <div>
-
+        <CommentInput />
       </div>
     );
   }
