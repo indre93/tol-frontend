310bd45492608f377d47fcd345e70ecd31824c6a
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-05 20:30:23 -0600

    add datePosted arrow function to format created_at attribute received from backend

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 const Comments = (props) => {
 
+  const datePosted = (created_date) => {
+    const formatDate = new Date(`${created_date}`);
+    const date = formatDate.toLocaleString();
+    return date;
+  };
+
   return (
     <div>
       <h3>Comments: </h3>
       {props.comments && props.comments.map(comment =>
         <li key={comment.id}>
-          {comment.content} - By {comment.username}
+          {comment.content} - By {comment.username} - Posted: {datePosted(comment.created_at)}
         </li>
       )}
     </div>
