30262fc10c18293baeac493ab2e94dea11f437ae
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-10 03:45:45 -0600

    import datePosted function and remove arrow function

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,23 +1,18 @@
 import React from 'react';
+import { datePosted } from '../FormatCreatedAt';
 
 const Comments = (props) => {
+    return (
+        <div>
 
-  const datePosted = (created_date) => {
-    const formatDate = new Date(`${created_date}`);
-    const date = formatDate.toLocaleString();
-    return date;
-  };
-
-  return (
-    <div>
-      <h3>Comments: </h3>
-      {props.comments && props.comments.map(comment =>
-        <li key={comment.id}>
-          {comment.content} - By {comment.username} - Posted: {datePosted(comment.created_at)}
-        </li>
-      )}
-    </div>
-  );
+            <h3>Comments: </h3>
+            {props.comments && props.comments.map(comment =>
+                <li key={comment.id}>
+                    {comment.content} - By {comment.username} - Posted: {datePosted(comment.created_at)}
+                </li>
+            )}
+        </div>
+    );
 };
 
 export default Comments;
