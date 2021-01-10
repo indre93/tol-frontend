de31ffc459e4c67a60a16c2080417186b950b9da
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-10 03:48:12 -0600

    add style using Card component from react-bootstrap

--- src/components/Discussions/Discussions.js
+++ src/components/Discussions/Discussions.js
@@ -5,18 +5,28 @@ import './Discussions.css';
 import Card from 'react-bootstrap/Card';
 
 const Discussions = (props) => {
-  return (
-    <div>
-      <h1>Discussions</h1>
-      {props.discussions.map(discussion =>
-        <li key={discussion.id}>
-          <Link to={`/discussions/${discussion.id}`}>
-            {discussion.topic}
-          </Link>
-        </li>
-      )}
-    </div>
-  );
+    return (
+        <div className="DiscussionsContainer">
+            <h2>Discussions</h2>
+            {props.discussions.map(discussion =>
+                <Card key={discussion.id} className="mb-2">
+                    <Card.Body>
+                        <Card.Header as="h6" className="mb-2 text-muted">
+                            {datePosted(discussion.created_at)}
+                        </Card.Header>
+                        <Card.Title>
+                            <Link to={`/discussions/${discussion.id}`}>
+                                {discussion.topic}
+                            </Link>
+                        </Card.Title>
+                        <Card.Subtitle className="text-muted">
+                            {discussion.comments.length} Comment(s)
+                    </Card.Subtitle>
+                    </Card.Body>
+                </Card>
+            )}
+        </div>
+    );
 };
 
 export default Discussions;
