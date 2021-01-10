978f80e07be18bfddde651170527a3770ce1a426
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-10 03:36:13 -0600

    change the order in which the links are being displayed

--- src/components/NavOptions.js
+++ src/components/NavOptions.js
@@ -5,27 +5,29 @@ import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 
 const NavOptions = () => {
-  return (
-    <div className="navButtons">
-      <Nav justify variant="pills" >
-        <Nav.Item>
-          <NavLink to='/'>
-            <Button variant="outline-light" size="lg">Home</Button>
-          </NavLink>
-        </Nav.Item>
-        <Nav.Item>
-          <NavLink to='/discussions/new'>
-            <Button variant="outline-light" size="lg">Add New Discussion</Button>
-          </NavLink>
-        </Nav.Item>
-        <Nav.Item>
-          <NavLink to='/discussions'>
-            <Button variant="outline-light" size="lg">All Discussions</Button>
-          </NavLink>
-        </Nav.Item>
-      </Nav>
-    </div>
-  );
+    return (
+        <div className="navButtons">
+            <Nav justify variant="pills" >
+                <Nav.Item>
+                    <NavLink to='/'>
+                        <Button variant="outline-light" size="lg">Home</Button>
+                    </NavLink>
+                </Nav.Item>
+
+                <Nav.Item>
+                    <NavLink to='/discussions'>
+                        <Button variant="outline-light" size="lg">All Discussions</Button>
+                    </NavLink>
+                </Nav.Item>
+
+                <Nav.Item>
+                    <NavLink to='/discussions/new'>
+                        <Button variant="outline-light" size="lg">Add New Discussion</Button>
+                    </NavLink>
+                </Nav.Item>
+            </Nav>
+        </div>
+    );
 };
 
 export default NavOptions;
