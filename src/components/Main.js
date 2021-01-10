dc43785520f43269adfe4ccb6924d89fffe06dda
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-10 03:33:37 -0600

    remove unnecessary div and update h1

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,17 +4,15 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import NavOptions from './NavOptions';
 
 const Main = () => {
-  return (
-    <div>
-      <Jumbotron fluid>
-        <div className="headerText">
-          <h1>Welcome to TOL</h1>
-          <p>A place where you can connect with others, share your thoughts, and discuss various topics</p>
-        </div>
-        <NavOptions />
-      </Jumbotron>
-    </div>
-  );
+    return (
+        <Jumbotron fluid>
+            <div className="headerText">
+                <h1>TOL</h1>
+                <p>A place where you can connect with others, share your thoughts, and discuss various topics</p>
+            </div>
+            <NavOptions />
+        </Jumbotron>
+    );
 };
 
 export default Main;
