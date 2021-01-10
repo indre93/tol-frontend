f5ff0fd5c23f82eec64e18998b2cc2e0bc4afe30
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-10 16:06:53 -0600

    import DiscussionInput.css for styling

--- src/components/Discussions/DiscussionInput.js
+++ src/components/Discussions/DiscussionInput.js
@@ -1,44 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addDiscussion } from '../../actions/discussions';
+import './DiscussionInput.css';
 
 class DiscussionInput extends Component {
 
-  state = {
-    topic: ''
-  };
+    state = {
+        topic: ''
+    };
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.addDiscussion(this.state);
-    this.setState({
-      topic: ''
-    });
-  };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.addDiscussion(this.state);
+        this.setState({
+            topic: ''
+        });
+    };
 
-  render() {
-    return (
-      <div>
-        <h1>Start a New Discussion</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>Topic: </label>
-          <input
-            type='text'
-            name='topic'
-            value={this.state.topic}
-            onChange={this.handleChange}
-          />
-          <input type='submit' />
-        </form>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Start a New Discussion</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Topic: </label>
+                    <input
+                        type='text'
+                        name='topic'
+                        value={this.state.topic}
+                        onChange={this.handleChange}
+                    />
+                    <input type='submit' />
+                </form>
+            </div>
+        );
+    }
 }
 
 export default connect(null, { addDiscussion })(DiscussionInput);
