4247886ac6950d4595706a4d54a8eae60104e625
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-09 23:42:27 -0600

    add styling both using react-bootstrap and css

--- src/components/NavOptions.js
+++ src/components/NavOptions.js
@@ -2,21 +2,28 @@ import React from 'react';
 import './NavOptions.css';
 import { NavLink } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
 
 const NavOptions = () => {
   return (
-    <div>
-      <nav className='navButtons'>
-        <NavLink to='/'>
-          <Button variant="outline-light" size="lg">Home</Button>
-        </NavLink>
-        <NavLink to='/discussions/new'>
-          <Button variant="outline-light" size="lg">Start Discussion</Button>
-        </NavLink>
-        <NavLink to='/discussions'>
-          <Button variant="outline-light" size="lg">All Discussions</Button>
-        </NavLink>
-      </nav>
+    <div className="navButtons">
+      <Nav justify variant="pills" >
+        <Nav.Item>
+          <NavLink to='/'>
+            <Button variant="outline-light" size="lg">Home</Button>
+          </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+          <NavLink to='/discussions/new'>
+            <Button variant="outline-light" size="lg">Add New Discussion</Button>
+          </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+          <NavLink to='/discussions'>
+            <Button variant="outline-light" size="lg">All Discussions</Button>
+          </NavLink>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 };
