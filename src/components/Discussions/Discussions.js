70a8b0351926a5c900820c674f575f8719e8646a
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-11 00:50:15 -0600

    restructure the way all discussions are being displayed

--- src/components/Discussions/Discussions.js
+++ src/components/Discussions/Discussions.js
@@ -6,24 +6,26 @@ import Card from 'react-bootstrap/Card';
 
 const Discussions = (props) => {
     return (
-        <div className={style.DiscussionsContainer}>
-            <h2>Discussions</h2>
+        <Card className={style.cardContainer}>
+            <Card.Header>
+                <h2>Discussion Topics</h2>
+            </Card.Header>
             {props.discussions.map(discussion =>
-                <Card key={discussion.id} className={`mb-2 ${style.card}`}>
-                    <Card.Body>
-                        <Card.Header as="h6" className={`mb-2 ${style.textMuted}`}>
+                <div key={discussion.id} className={`mb-2 ${style.card}`}>
+                    <Card.Body className={style.cardBody}>
+                        <Card.Subtitle variant="top" className={`mb-2 ${style.textMuted}`}>
                             {datePosted(discussion.created_at)}
-                        </Card.Header>
+                        </Card.Subtitle>
                         <Card.Title>
                             <Link to={`/discussions/${discussion.id}`}>{discussion.topic}</Link>
                         </Card.Title>
-                        <Card.Subtitle className={`mb-2 ${style.textMuted}`}>
+                        <Card.Subtitle className={` ${style.textMuted}`}>
                             {discussion.comments.length} Comment(s)
                         </Card.Subtitle>
                     </Card.Body>
-                </Card>
+                </div>
             )}
-        </div>
+        </Card>
     );
 };
 
