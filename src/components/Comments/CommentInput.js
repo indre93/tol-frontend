4ef51f13b5b1f10b2b37263ad10179530029eb0d
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-12 00:47:23 -0600

    add boostrap styling

--- src/components/Comments/CommentInput.js
+++ src/components/Comments/CommentInput.js
@@ -3,54 +3,47 @@ import { connect } from 'react-redux';
 import { addComment } from '../../actions/comments';
 import style from './CommentInput.module.css';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
 class CommentInput extends Component {
 
-  state = {
-    username: '',
-    content: ''
-  };
+    state = {
+        username: "",
+        content: ""
+    };
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.addComment(this.state, this.props.discussion.id);
-    this.setState({
-      username: '',
-      content: ''
-    });
-  };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.addComment(this.state, this.props.discussion.id);
+        this.setState({
+            username: "",
+            content: ""
+        });
+    };
 
-  render() {
-    return (
-      <div>
-        <h3>Add Comment</h3>
-        <form onSubmit={this.handleSubmit}>
-          <label>Username: </label>
-          <input
-            type='text'
-            name='username'
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-          <label>Comment: </label>
-          <input
-            type='text'
-            name='content'
-            value={this.state.content}
-            onChange={this.handleChange}
-          />
-          <input type='submit' />
-        </form>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={style.formContainer}>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Control
+                        type="text"
+                        name="content"
+                        placeholder="Enter comment"
+                        className={style.formInput}
+                        value={this.state.content}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <button className={style.formBtn} type="submit">Submit</button>
+                </Form>
+            </div>
+        );
+    }
 }
 
 export default connect(null, { addComment })(CommentInput);
