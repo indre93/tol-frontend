41f87b7b2f4ba8b86a674cca41319260a0ba30b9
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-17 22:12:22 -0600

    import col from react bootstrap

--- src/components/Comments/CommentInput.js
+++ src/components/Comments/CommentInput.js
@@ -4,6 +4,7 @@ import { addComment } from '../../actions/comments';
 import style from '../../style/comments/CommentInput.module.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 
 class CommentInput extends Component {
 
