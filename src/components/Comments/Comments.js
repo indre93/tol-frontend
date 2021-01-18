694c24d0fe2154ddf653fa4a874e281edcb88ada
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-17 18:50:16 -0600

    update spacing

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,19 +4,19 @@ import style from '../../style/comments/Comments.module.css';
 import Card from 'react-bootstrap/Card';
 
 const Comments = (props) => {
-    return (
-        <div className={style.container}>
-            {props.comments && props.comments.map(comment =>
-                <div key={comment.id} className={style.comment}>
-                    <Card.Header className={style.commentHeader}>
-                        <strong className={style.username}>{comment.username}</strong>
-                        <small className={style.postedTime}>{datePosted(comment.created_at)}</small>
-                    </Card.Header>
-                    <Card.Text className={style.cardBody}>{comment.content}</Card.Text>
-                </div>
-            )}
+  return (
+    <div className={style.container}>
+      {props.comments && props.comments.map(comment =>
+        <div key={comment.id} className={style.comment}>
+          <Card.Header className={style.commentHeader}>
+            <strong className={style.username}>{comment.username}</strong>
+            <small className={style.postedTime}>{datePosted(comment.created_at)}</small>
+          </Card.Header>
+          <Card.Text className={style.cardBody}>{comment.content}</Card.Text>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Comments;
