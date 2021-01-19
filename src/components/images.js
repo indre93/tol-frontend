b4291de3e6f24aeba253e3beb92a6dc10b3902c7
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-18 20:56:26 -0600

    add images component to import images and create  variable w object to be used in welcome page

---
+++ src/components/images.js
@@ -0,0 +1,11 @@
+import slide1 from '../style/images/slide1.jpg';
+import slide2 from '../style/images/slide2.jpg';
+import slide3 from '../style/images/slide3.jpg';
+
+const images = [
+  { src: `${slide1}`, title: "Socrates (469-399 B.C.)" },
+  { src: `${slide2}`, title: "Plato (428-347 B.C.)" },
+  { src: `${slide3}`, title: "Aristotle (384-322 B.C.)" },
+];
+
+export default images;
