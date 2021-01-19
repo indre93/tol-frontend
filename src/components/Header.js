550c644ff018ada2daaa6aa7ed30188f06b8adc9
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-18 20:58:58 -0600

    update main name to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import '../style/Main.css';
+import '../style/Header.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import NavOptions from './NavOptions';
 
-const Main = () => {
+const Header = () => {
   return (
     <Jumbotron fluid>
       <div className="headerText">
         <h1>TOL</h1>
-        <p>A place where you can connect with others, share your thoughts, and discuss various topics</p>
+        <p>a place where you can connect with others, share your thoughts, and discuss various topics</p>
       </div>
       <NavOptions />
     </Jumbotron>
   );
 };
 
-export default Main;
+export default Header;
