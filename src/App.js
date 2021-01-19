b1b1f680e7911e7af57af0ac4084cf109b9f7871
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-18 21:00:51 -0600

    update file and component name

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './style/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route } from 'react-router-dom';
-import Main from './components/Main';
+import Header from './components/Header';
 import DiscussionsContainer from './containers/DiscussionsContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App" >
-        <Route path="/" component={Main}></Route>
+        <Route path="/" component={Header}></Route>
         <Route path="/discussions" component={DiscussionsContainer}></Route>
       </div>
     );
