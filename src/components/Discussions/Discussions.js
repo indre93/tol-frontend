fefa7e974f6e4fd000a052428cf973417f0ee4c9
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-28 00:17:25 -0600

    import LikeButton functional component to be used for each Discussion

--- src/components/Discussions/Discussions.js
+++ src/components/Discussions/Discussions.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { datePosted } from '../FormatCreatedAt';
 import style from '../../style/discussions/Discussions.module.css';
+import LikeButton from './LikeButton';
 
 const Discussions = (props) => {
+
   return (
     <div className={style.container}>
       <h2>Discussion Topics</h2>
-
       <div className={style.containerBody}>
-        {props.discussions.map(discussion =>
+
+        {props.discussions.map((discussion) =>
           <div key={discussion.id} className={style.bodyContent}>
             <div className={style.containerDate}>
               {datePosted(discussion.created_at)}
@@ -19,11 +21,13 @@ const Discussions = (props) => {
             </div>
             <div className={style.containerComments}>
               {discussion.comments.length} Comment(s)
+              <LikeButton />
             </div>
           </div>
         )}
+
       </div>
-    </div>
+    </div >
   );
 };
 
